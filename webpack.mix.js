20d340645ff899ae7cd292313a6ee14238b8abfb
Alexandru Noroc <kradores10@gmail.com>
2019-08-24 14:53:12 +0300

    ADDED: adminlte

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,14 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .styles([
+    'public/css/app.css',
+    'vendor/almasaeed2010/adminlte/dist/css/adminlte.css',
+    'vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.css'
+    ], 'public/css/all.css')
+    .scripts([
+        'vendor/almasaeed2010/adminlte/plugins/jquery/jquery.js',
+        'vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.js',
+        'vendor/almasaeed2010/adminlte/dist/js/adminlte.js'
+    ], 'public/js/all.js');
